617daec04432e3bc79d9821ad41428a872b520c6
julien gourdon <jul.gourdon@gmail.com>
2020-07-09 11:24:22 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorTestTimePicker/Shared/TimespanExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorTestTimePicker.Shared
+{
+    public static class TimespanExtension
+    {
+        public static DateTime ToDateTime(this TimeSpan source)
+        {
+            return DateTime.Now.Date + source;
+        }
+
+        public static TimeSpan RoundToNearestMinutes(this TimeSpan input, int minutes)
+        {
+            var totalMinutes = (int)(input + new TimeSpan(0, minutes / 2, 0)).TotalMinutes;
+
+            return new TimeSpan(0, totalMinutes - totalMinutes % minutes, 0);
+        }
+    }
+}
